4dd5ad7c760939c1cc5af5b9712b6389a5826580
ardipermana59 <62947509+ardipermana59@users.noreply.github.com>
2020-06-27 13:57:26 +0700

    add click scroll

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,26 @@ const mobile = document.querySelector(".mobile");
 mobile.addEventListener('click', () => {
 	alert("maaf belum tersedia :(")
 });
+
+const home = document.querySelector(".scroll-home");
+home.addEventListener('click', function (e){
+		e.preventDefault();
+	document.querySelector("body").scrollIntoView({behavior: 'smooth'});
+});
+
+const contact = document.querySelector(".scroll-contact");
+contact.addEventListener('click', function (e){
+		e.preventDefault();
+	document.querySelector("#contact").scrollIntoView({behavior: 'smooth'});
+});
+
+const about = document.querySelector(".scroll-about");
+about.addEventListener('click', function (e){
+		e.preventDefault();
+	document.querySelector("#offer").scrollIntoView({behavior: 'smooth'});
+});
+
+const btnLogin = document.querySelector(".login");
+btnLogin.addEventListener('click', function(){
+	alert('Maaf menu belum tersedia');
+})
